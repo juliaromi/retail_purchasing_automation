910df8e0cb0c3e070b66e7d626d780c63649269d
Yulia M <julia0205ro@gmail.com>
2025-04-27 16:39:58 +0300

    Edided user model

--- orders/backend/models.py
+++ orders/backend/models.py
@@ -8,27 +8,38 @@ from django.db import models
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, name, lastname, login, password):
-        if not name:
-            raise ValidationError('Name must be provided')
-        if not lastname:
-            raise ValidationError('Lastname must be provided')
+    def create_user(self, login, password, **extra_fields):
         if not login:
             raise ValidationError('Email address must be provided')
         if not password:
             raise ValidationError('Password must be provided')
-        user = self.model(name=name, lastname=lastname, login=login)
+        user = self.model(login=login, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_staff(self, name, lastname, login, password):
-        staff_user = self.create_user(name, lastname, login, password)
-        staff_user.is_staff = True
+    def create_staff(self, login, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        staff_user = self.create_user(login, password, **extra_fields)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValidationError('User must be staff')
+
         staff_user.save(using=self._db)
 
         return staff_user
+
+    def create_superuser(self, login, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        admin_user = self.create_user(login, password, **extra_fields)
+        admin_user.save(using=self._db)
+
+        if (extra_fields.get('is_staff') and extra_fields.get('is_staff')) is not True:
+            raise ValidationError('User must be superuser')
+
+        return admin_user
 
 
 class User(AbstractUser):
@@ -40,10 +51,11 @@ class User(AbstractUser):
     lastname= models.CharField(max_length=50, blank=False)
     login = models.EmailField(unique=True, blank=False, null=False)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'login'
-    REQUIRED_FIELDS = ['name', 'lastname', 'contact']
+    REQUIRED_FIELDS = ['name', 'lastname']
 
     objects = UserManager()
 
